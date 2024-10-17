5754e995e639651058b2a65471dbc7d44749f622
sharonwav <2082023@unai.edu>
2024-10-17 14:52:03 +0700

    Settingan awal server

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const port = 1000;
+app.get('/', (req, res) => {
+    res.send('<p>You are on the server!</p>');
+});
+app.listen(port, () => {
+    console.log(`You are on the server http://localhost:${port}`);
+});
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+app.use(express.json())
+const port = 1000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('<p>You are on the server!</p>')
+})
+
+app.listen(port, () => {
+    console.log(`You are on the server http://localhost:${port}`);
+})
